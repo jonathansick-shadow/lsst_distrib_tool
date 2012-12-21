1467f293a631f1b73916600f01d73bd190daeaa6
Mario Juric <mjuric@lsst.org>
2012-12-21 00:01:58 -0700

    Send rebuild logs to logs/

--- scripts/rebuild/rebuild.sh
+++ scripts/rebuild/rebuild.sh
@@ -10,5 +10,6 @@ HOST=$(head -n1 "$OS/host")
 echo "Building platform $OS on $HOST..."
 rsync -az $OS/ $HOST:/opt/lsst/$OS.buildscripts || { echo "Failed to rsync build script to $HOST"; exit -1; }
 ssh $HOST "cd /opt/lsst/$OS.buildscripts && bash rebuild.sh $RELEASE_TAG $DISTRIB_TOOL_VER > build.log 2>&1"
-scp -q $HOST:/opt/lsst/$OS.buildscripts/build.log build.$OS.log
-echo "Build done for $OS on $HOST. Log is in build.$OS.log"
+mkdir -p logs
+scp -q $HOST:/opt/lsst/$OS.buildscripts/build.log logs/build.$OS.log
+echo "Build done for $OS on $HOST. Log is in logs/build.$OS.log"

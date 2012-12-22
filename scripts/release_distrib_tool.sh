10dcfb638a81f975c96856f0ac00904ecdf12d75
Mario Juric <mjuric@lsst.org>
2012-12-21 22:55:29 -0700

    Added release_distrib_tool.sh

---
+++ scripts/release_distrib_tool.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#
+# Release a new version of the distrib tool, and install it into the binary
+# distributions, all in one go. See the README for step-by-step explanation.
+#
+
+set -e
+
+./release_distrib_tool_package.sh
+
+(
+  cd rebuild &&
+  source ../config.sh &&
+  ./exec_cmd.sh "eups distrib install --nolocks lsst_distrib_tool $DISTRIB_TOOL_VER" $DISTROS
+  ./exec_cmd.sh "eups declare -c lsst_distrib_tool $DISTRIB_TOOL_VER" $DISTROS
+)
+
+./release_distrib_tool_webscript.sh
+
+(
+  cd rebuild &&
+  source ../config.sh &&
+  ./rsync_distros.sh $DISTROS
+)

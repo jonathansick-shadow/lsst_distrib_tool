8628082a4f277b85d51142a5c882ac78cd207a39
Mario Juric <mjuric@lsst.org>
2012-12-22 18:02:30 -0700

    config.sh: point $REPO to LSST/DMS/lsst_distrib_tool.git

--- scripts/config.sh
+++ scripts/config.sh
@@ -1,7 +1,7 @@
 # To be sourced by various scripts
 
 export DISTROS="osx-10.7-x86_64 osx-10.8-x86_64 rhel-6-x86_64 rhel-5-x86_64"
-export REPO=git@git.lsstcorp.org:contrib/lsst_distrib_tool.git
+export REPO=git@git.lsstcorp.org:LSST/DMS/lsst_distrib_tool.git
 export PKGDIR="/lsst/home/lsstsw/distrib/default/www/lsst_distrib_tool"
 export RELEASE_TAG=v6_1
 export DISTRIB_TOOL_TAG=${DISTRIB_TOOL_TAG-0.7}

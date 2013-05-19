0c98b80c1ba91eda4d189bd6a58e29bc7d84f53f
Mario Juric <mjuric@lsst.org>
2013-05-18 17:02:22 -0700

    Bump version to 0.12, bump RELESE_TAG to v6_2
    $RELESE_TAG is used by rebuild scripts when rebuilding the binary
    repositories from scratch.

--- etc/config.sh
+++ etc/config.sh
@@ -3,8 +3,8 @@
 export DISTROS="osx-10.7-x86_64 osx-10.8-x86_64 rhel-6-x86_64 rhel-5-x86_64 ubuntu-1204-x86_64"
 export REPO=git@git.lsstcorp.org:LSST/DMS/lsst_distrib_tool.git
 export PKGDIR="/lsst/home/lsstsw/distrib/default/www/lsst_distrib_tool"
-export RELEASE_TAG=v6_1
-export DISTRIB_TOOL_TAG=${DISTRIB_TOOL_TAG-0.11}
+export RELEASE_TAG=v6_2
+export DISTRIB_TOOL_TAG=${DISTRIB_TOOL_TAG-0.12}
 export DISTURL="moya.dev.lsstcorp.org:/var/www/html/dmstack/binaries"
 
 export DISTRIB_TOOL_VER="${DISTRIB_TOOL_TAG}+1"
